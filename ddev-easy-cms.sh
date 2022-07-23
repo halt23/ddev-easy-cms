66b902c0da6e7adfafb2af02e0abc6601b140ec6
halt23 <maassam495@gmail.com>
2022-07-23 21:04:52 +0200

    main source online

---
+++ ddev-easy-cms.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+initial_setup(){
+
+	if ! command -v docker >/dev/null; then
+		echo "########################################################################"
+		echo "This script requires docker to be installed: please install docker first"
+		echo "########################################################################"
+		exit 1
+
+	else 
+		echo "docker is installed.."
+	fi
+
+	if ! command -v ddev >/dev/null; then
+		echo "####################################################################"
+		echo "This script requires ddev to be installed: please install ddev first"
+		echo "####################################################################"
+		exit 1
+
+	else
+		echo "ddev is installed.."
+
+	fi
+
+if ! command -v zenity >/dev/null; then
+		echo "########################################################################"
+		echo "This script requires zenity to be installed: please install zenity first"
+		echo "########################################################################"
+		exit 1
+
+	else
+		echo "zenity is installed.."
+
+	fi
+
+	echo "continuing..."
+	sleep 2
+
+
+}
+
+setup_vars(){
+
+	dir=`zenity --entry --text="enter name of project directory(do not enter a path)" --title="project directory"`
+	path=`zenity --file-selection --directory --title="Select location to initialize the project directory $dir"`
+	cms=`zenity --forms --title "Select cms" --add-combo "chose cms." --combo-values "Typo3|Drupal|Wordpress"`
+	path_dir="$path/$dir"
+
+}
+
+
+setup_cms(){
+
+	if [[  $cms = "Wordpress" ]]; then
+
+		mkdir -p $path_dir
+		cd $path_dir
+		ddev config --project-type=wordpress
+		ddev start
+		ddev wp core download
+		ddev launch
+		
+	fi
+
+	if [[ $cms = "Drupal" ]]; then
+	
+		mkdir -p $path_dir
+		cd $path_dir
+		ddev config --project-type=drupal9 --docroot=web --create-docroot
+		ddev start
+		ddev composer create "drupal/recommended-project" --no-install
+		ddev composer require drush/drush --no-install
+		ddev composer install
+		ddev drush site:install -y
+		ddev drush uli
+		ddev launch
+
+	fi
+
+
+	if [[ $cms = "Typo3" ]]; then
+	
+		mkdir -p $path_dir
+		cd $path_dir
+		ddev config --project-type=typo3 --docroot=public --create-docroot
+		ddev start
+		ddev composer create "typo3/cms-base-distribution:^11"
+		cd public
+		touch FIRST_INSTALL
+
+	fi
+
+
+}
+initial_setup
+setup_vars
+setup_cms
